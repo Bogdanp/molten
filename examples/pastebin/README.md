d106a3dfec2ffa45f2064913faf89261bb7857b5
Bogdan Popa <popa.bogdanp@gmail.com>
2018-06-11 18:47:20 +0300

    chore: add pastebin example

---
+++ examples/pastebin/README.md
@@ -0,0 +1,13 @@
+# Pastebin Example
+
+A simple pastebin built with molten.
+
+## Usage
+
+Install molten with `poetry develop` then run the app with `gunicorn app:app`.
+
+## Example requests
+
+    $ curl http://127.1:8000
+    $ curl --data-binary @example.txt http://127.1:8000
+    $ curl http://127.1:8000/bc742daa-1d3c-46bb-a0b7-5338868256f7
---
+++ examples/pastebin/app.py
@@ -0,0 +1,62 @@
+"""\
+Usage:
+
+  POST /
+
+    Stores raw data from the request and returns a URI where the data
+    can be retrieved from.
+
+    $ curl --data-binary @file.txt http://127.1:8000
+
+  GET /{paste_id}
+
+    Returns the data for a paste.
+"""
+import os
+import uuid
+
+from molten import HTTP_200, HTTP_404, App, Request, Response, Route
+
+BUFSIZE = 1024 * 16
+
+
+def generate_paste_id():
+    return str(uuid.uuid4())
+
+
+def relative_path(*segments):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *segments)
+
+
+def index() -> Response:
+    return Response(HTTP_200, content=__doc__, headers={
+        "content-type": "text/plain",
+    })
+
+
+def upload(request: Request) -> str:
+    paste_id = generate_paste_id()
+    with open(relative_path("uploads", paste_id), "wb") as paste:
+        while True:
+            buff = request.body_file.read(BUFSIZE)
+            if not buff:
+                break
+
+            paste.write(buff)
+
+    return f"{request.scheme}://{request.host}:{request.port}/{paste_id}"
+
+
+def get_paste(paste_id: str) -> Response:
+    try:
+        paste = open(relative_path("uploads", paste_id), "rb")
+        return Response(HTTP_200, stream=paste)
+    except FileNotFoundError:
+        return Response(HTTP_404, content="Paste not found.")
+
+
+app = App(routes=[
+    Route("/", index),
+    Route("/", upload, method="POST"),
+    Route("/{paste_id}", get_paste),
+])
