958a51b4be225b1076c129824f52ca79d3731524
Bogdan Popa <popa.bogdanp@gmail.com>
2018-07-25 19:11:23 +0300

    chore: bump version 0.3.0 → 0.3.1

--- molten/__init__.py
+++ molten/__init__.py
@@ -41,7 +41,7 @@ from .validation import (
     Field, Missing, Validator, dump_schema, field, is_schema, load_schema, schema
 )
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = [
     "BaseApp", "App", "Middleware", "annotate",
