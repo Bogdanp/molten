d494c5220821d7c7ae43add573f6a5b9d1ecd3de
Bogdan Popa <bogdan@defn.io>
2019-10-28 14:53:00 +0200

    ci: stop running mypy during lint step

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,14 +4,3 @@ set -euo pipefail
 
 echo "Running flake8..."
 flake8 molten tests examples
-
-echo "Running mypy..."
-mypy --ignore-missing-imports \
-     --disallow-any-unimported \
-     --disallow-any-generics \
-     --disallow-untyped-calls \
-     --disallow-untyped-defs \
-     --disallow-incomplete-defs \
-     --disallow-subclassing-any \
-     --no-implicit-optional \
-     molten molten/contrib
